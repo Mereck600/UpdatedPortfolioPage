95ab3c87e2e2be8e4adef60a2c956d04d077ee46
Mereck600 <160926474+Mereck600@users.noreply.github.com>
2025-09-22 13:42:39 -0400

    if i have one more image issue i will never touch nextjs again in my life:

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,13 @@
 // next.config.mjs
-/** @type {import('next').NextConfig} */
 const isProd = process.env.NODE_ENV === 'production';
-const repo = 'UpdatedPortfolioPage'; // exact repo name
+const repo = 'UpdatedPortfolioPage';
 
-const nextConfig = {
+export default {
   reactStrictMode: true,
   output: 'export',
   basePath: isProd ? `/${repo}` : '',
   images: { unoptimized: true },
-
-  // for raw <img>, <link rel="icon">, CSS url(), etc.
   env: {
     NEXT_PUBLIC_BASE_PATH: isProd ? `/${repo}` : '',
   },
 };
-
-export default nextConfig;
